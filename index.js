78d79e36e72c570412c8b342341a2efa46e9036c
HemendraRoy <158675218+HemendraRoy@users.noreply.github.com>
2024-08-31 16:16:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+let submission=document.getElementById('submit');
+submission.addEventListener('click',getdata);
+const API_KEY = "AIzaSyCEe6wmdneDr35xKfxsAacYrVuQzERdjjE"
+const ai=new GoogleGenerativeAI(process.env.API_KEY)
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+
+
+function getdata(){
+	let pretext=document.getElementById('hobbies');
+	let keywords=pretext.split(',');
+	let text="Show me list of career options if my interests are in ";
+	for(str in keywords){
+		text+=str;
+		text+=', ';
+	}
+	
+	const res=await model.generateContent(text);
+	let fin=res.response().text();
+	document.getElementById('options').innerHTML=fin;
+}
